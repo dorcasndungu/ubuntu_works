b4d77d4ae41f3bcf3ad3e39473b4613f8ce282d3
njiti <kamaukan77@gmail.com>
2024-05-09 13:15:08 +0300

    finished on front_end

---
+++ src/app/right/right.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right',
+  standalone: true,
+  imports: [],
+  templateUrl: './right.component.html',
+  styleUrl: './right.component.scss'
+})
+export class RightComponent {
+
+}
